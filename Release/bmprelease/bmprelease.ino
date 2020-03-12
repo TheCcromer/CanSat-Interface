f83cf2adfbac13fa00f7b0c04414d5aec87ca142
alvarobermudez04 <60280444+alvarobermudez04@users.noreply.github.com>
2020-03-11 18:58:24 -0600

    Add files via upload

---
+++ Release/bmprelease/bmprelease.ino
@@ -0,0 +1,53 @@
+
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+
+Adafruit_BMP280 bmp; // I2C
+//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+
+void setup() {
+  pinMode(2,OUTPUT);
+  Serial.begin(9600);
+  Serial.println(F("BMP280 test"));
+
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+}
+
+H_anterior = 0;
+H_actual = 0
+
+void loop() {
+    
+    Serial.print(bmp.readTemperature());
+
+    Serial.print(bmp.readPressure());
+
+
+    H_actual = bmp.readAltitude(1013.25)
+
+    if (H_actual>100 && H_actual>H_anterior && H_actual<450);
+    {
+      digitalWrite(2,HIGH);
+    }
+    H_anterior = H_actual
+    Serial.println();
+    delay(2000);
+}
